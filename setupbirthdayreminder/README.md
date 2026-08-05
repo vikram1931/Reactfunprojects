dbe7e7e853f0a8b97df0aa320a36187b0328629a
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-05 21:29:16 +0530

    Add files via upload

---
+++ setupbirthdayreminder/README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# ReactBasicProjects
+=======
+## Idea
+
+[https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
+>>>>>>> 24950a7 (birthday reminder)
---
+++ setupbirthdayreminder/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  return <h2>reminder project setup</h2>;
+}
+
+export default App;
+
+// this is an example
