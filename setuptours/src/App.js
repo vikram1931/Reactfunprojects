e3843a21ecd1615287ff4200e36435b77bd40ad6
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-06 20:39:12 +0530

    finished tours

--- setuptours/src/App.js
+++ setuptours/src/App.js
@@ -1,11 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://jsonplaceholder.typicode.com/users";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      // console.log(tours);
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button onClick={fetchTours}> Refresh</button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
+
+/* import axios from "axios";
+
+async function getData() {
+  const response = await axios.get("/api/data");
+  return response.data;
+} */
+
+// async/await
+/*  async function getData() {
+  const response = await fetch("/api/data");
+  const json = await response.json();
+  return json;
+}*/
--- setuptours/src/Tour.js
+++ setuptours/src/Tour.js
@@ -1,7 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({
+  id,
+  name,
+  username,
+  address,
+  phone,
+  website,
+  company,
+  removeTour,
+}) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <div>
+      <h2>ID: {id}</h2>
+      <h2>Name:{name}</h2>
+      <h2>Username:{username}</h2>
+      <h2>
+        Address:
+        <h2>Street:{address.street}</h2>
+        <h2>suite:{address.suite}</h2>
+        <h2>city:{address.city}</h2>
+        <h2>Zipcode:{address.zipcode}</h2>
+        <h2>
+          Geo:
+          <h2>lat:{address.geo.lat}</h2>
+          <h2>lng: {address.geo.lng}</h2>
+        </h2>
+      </h2>
+
+      <h2>Phone:{phone}</h2>
+      <h2>website:{website}</h2>
+      <h2>
+        Company:
+        <h2>name:{company.name}</h2>
+        <h2>
+          catchPhrase:
+          {readMore
+            ? company.catchPhrase
+            : `${company.catchPhrase.substring(0, 5)}`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "ReadLess" : "ReadMore"}
+          </button>
+        </h2>
+        <h2>bs:{company.bs}</h2>
+      </h2>
+      <button className="delete-btn" onClick={() => removeTour(id)}>
+        Not interested
+      </button>
+    </div>
+  );
 };
 
 export default Tour;
+
+/* id,
+  name,
+  username,
+  email,
+  address: {
+    street,
+    suite,
+    city,
+    zipcode,
+    geo: { lat, lng },
+  },
+  phone,
+  website,
+  company: { name, catchPhrase, bs },*/
