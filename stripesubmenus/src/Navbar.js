66d7c55b179e77593049ea2fe8caa555113cbecd
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-15 11:30:51 +0530

    set up context and invoked it in various components

--- stripesubmenus/src/Navbar.js
+++ stripesubmenus/src/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const [openSidebar, openSubmenu, closeSubmenu] = useGlobalContext();
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button
+            type="button"
+            className="btn toggle-btn"
+            onClick={openSidebar}></button>
+        </div>
+      </div>
+      <ul className="nav-links"></ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
