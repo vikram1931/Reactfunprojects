35676e45944538a8e0bb2c6460da56929abfeb39
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-16 14:20:28 +0530

    setting focus and handle submit to prevent default

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -5,12 +5,19 @@ const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef("");
 
+  React.useEffect(() => {
+    searchValue.current.focus();
+  });
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
           <input
