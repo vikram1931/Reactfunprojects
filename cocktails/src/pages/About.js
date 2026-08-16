6ec13a9fbc2782884f46c628084f5294b66e0854
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-16 13:43:58 +0530

    routing and fetching api

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
